d3e05d372bc8a6d909e441959ea9fbc4d0d7d963
BannedForN <bukreev04@mail.ru>
2024-06-13 19:18:33 +0300

    Initial commit

---
+++ WpfApp10/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Net.Mail;
+using System.Windows;
+using System.Windows.Controls;
+using Spire.Xls;
+
+namespace WpfApp10
+{
+    public partial class MainWindow : Window
+    {
+        public static string currentFilePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a new Excel file
+            dataGrid.ItemsSource = null;
+            dataGrid.Columns.Clear();
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open an existing Excel file
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Excel Files|*.xls;*.xlsx"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                currentFilePath = openFileDialog.FileName;
+                Workbook excelFile = new Workbook();
+                excelFile.LoadFromFile(currentFilePath);
+                Worksheet sheet = excelFile.Worksheets[0];
+                CellRange locatedRange = sheet.AllocatedRange;
+                var dataTable = sheet.ExportDataTable(locatedRange, true);
+                dataGrid.ItemsSource = dataTable.DefaultView;
+            }
+        }
+
+        private void SaveFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Save the current Excel file
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog
+                {
+                    Filter = "Excel Files|*.xlsx"
+                };
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    currentFilePath = saveFileDialog.FileName;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            var dataTable = dataGrid.ItemsSource as DataView;
+
+            Workbook excelFile = new Workbook();
+            excelFile.Worksheets.Clear();
+            Worksheet sheet = excelFile.Worksheets.Add("Лист 1");
+            sheet.InsertDataView(dataTable, true, 1,1);
+            excelFile.SaveToFile(currentFilePath, Spire.Xls.FileFormat.Version2016);
+        }
+
+        private void SendEmailButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendWindow send = new SendWindow();
+            send.ShowDialog();
+        }
+    }
+}
---
+++ WpfApp10/SendWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp10
+{
+    public partial class SendWindow : Window
+    {
+        public SendWindow()
+        {
+            InitializeComponent();
+        }
+        private void SendEmailButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Send the current Excel file via email
+            MailMessage mail = new MailMessage(FromTbx.Text, ToTbx.Text, TitleTbx.Text, "Content of the email");
+
+            Attachment attachment = new Attachment(MainWindow.currentFilePath);
+            mail.Attachments.Add(attachment);
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.Credentials = new System.Net.NetworkCredential(FromTbx.Text, PswTbx.Text);
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(mail);
+        }
+    }
+}
